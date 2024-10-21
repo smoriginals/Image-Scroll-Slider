7b249562ba1f841075627d597f2578a91516085b
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-10-21 21:51:27 +0530

    Update
    Optimization, 95% optimize

--- script.js
+++ script.js
@@ -22,30 +22,3 @@ function SlideCheck() {
 }
 
 window.addEventListener("scroll", SlideCheck);
-
-
-/*const slideImages = document.querySelectorAll(".slide-img");
-const rImg = document.querySelector(".right-side");
-const lImg = document.querySelector(".left-side");
-const mImg = document.querySelector(".mid-side");
-
-function SlideCheck(e) {
-    slideImages.forEach(slides => {
-        const slideAt = (window.scrollY + window.innerHeight) - slides.height / 2;
-        const bottomImg = slides.offsetTop + slides.height;
-        const halfScroll = slideAt > slides.offsetTop;
-        const scrollNone = window.scrollY < bottomImg;
-
-        if (halfScroll && scrollNone) {
-            rImg.classList.add("right-side-anim");
-            lImg.classList.add("left-side-anim");
-            mImg.classList.add("mid-side-anim");
-        } else {
-            rImg.classList.remove("right-side-anim");
-            lImg.classList.remove("left-side-anim");
-            mImg.classList.remove("mid-side-anim");
-        }
-    });
-}
-
-window.addEventListener("scroll", SlideCheck);*/
